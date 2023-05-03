119952a181ee61dc59a618265f71e177a4dc52aa
mios-tech <oscarsilva@mios.pt>
2023-05-03 10:48:57 +0100

    init

---
+++ style/StyleDark.js
@@ -0,0 +1,10 @@
+import { Platform, StyleSheet } from "react-native";
+const colors = require("../style/Colors.json");
+
+const style = StyleSheet.create({
+  backgroundPrimary: {
+    backgroundColor: colors.Base_Slot_1,
+  }
+});
+
+export default style;
